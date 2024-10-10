67c7049cc9c5a8e3d75b0392bee4b0031dad80fe
Vedavyas <vvyas.b@gmail.com>
2024-10-10 22:53:18 +0530

    Added Typed Js effect for a paragraph

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Engineer", "ML Enthusiast", "Developer"],
+  typeSpeed: 75,
+  backSpeed: 75,
+  backDelay: 600,
+  loop: true,
+});
